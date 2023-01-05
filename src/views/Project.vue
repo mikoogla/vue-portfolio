c54d98aa37d43ee9678dfbdefe06938a37d8bc9c
mikoogla <78367511+mikoogla@users.noreply.github.com>
2023-01-05 21:41:00 +0100

    warning text

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,155 +1,158 @@
 <script setup>
-import Button from "../UI/Button.vue";
-import { mapGetters } from "vuex";
+import Button from "../UI/Button.vue"
+import { mapGetters } from "vuex"
 </script>
 
 <template>
-  <div
-    class="project-container"
-    :style="{ backgroundImage: `url(${getCurrentProject.background})` }"
-  >
-    <div class="section">
-      <div class="title">
-        <h1>{{ getCurrentProject.title || "Failed" }}</h1>
-        <div class="description">
-          {{ getCurrentProject.description || "Failed" }}
-        </div>
-      </div>
+	<div
+		class="project-container"
+		:style="{ backgroundImage: `url(${getCurrentProject.background})` }"
+	>
+		<div class="section">
+			<div class="title">
+				<h1>{{ getCurrentProject.title || "Reading failed" }}</h1>
+				<div class="description">
+					{{
+						getCurrentProject.description ||
+						"Something went wrong. Go back to Homepage"
+					}}
+				</div>
+			</div>
 
-      <div class="button-container">
-        <Button @click="openSite">
-          <template #icon>
-            <span class="material-symbols-outlined"> link </span>
-          </template>
-          <template #text>{{ text.projectInfo.siteButton }}</template>
-        </Button>
-        <Button @click="openSource">
-          <template #icon>
-            <span class="material-symbols-outlined"> code </span>
-          </template>
-          <template #text>{{ text.projectInfo.sourceButton }}</template>
-        </Button>
-      </div>
-    </div>
-  </div>
+			<div class="button-container">
+				<Button @click="openSite">
+					<template #icon>
+						<span class="material-symbols-outlined"> link </span>
+					</template>
+					<template #text>{{ text.projectInfo.siteButton }}</template>
+				</Button>
+				<Button @click="openSource">
+					<template #icon>
+						<span class="material-symbols-outlined"> code </span>
+					</template>
+					<template #text>{{ text.projectInfo.sourceButton }}</template>
+				</Button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Project",
-  components: {
-    Button,
-  },
-  data() {
-    return {
-      id: this.$route.params.id,
-      project: {},
-    };
-  },
-  methods: {
-    openSite() {
-      window.open(this.getCurrentProject.siteLink, "_blank");
-    },
-    openSource() {
-      window.open(this.getCurrentProject.sourceLink, "_blank");
-    },
-  },
-  beforeMount() {},
-  onUpdated() {
-    if (this.getCurrentProject.title) {
-      console.log("Current Project");
-      this.project = this.getCurrentProject();
-    } else {
-      console.log("Project by name");
-      this.project = this.getProjectByName(this.$route.params.id);
-    }
-  },
-  computed: {
-    ...mapGetters(["getCurrentProject", "getProjectByName"]),
-    ...mapGetters({
-      text: "getTexts",
-    }),
-  },
-};
+	name: "Project",
+	components: {
+		Button,
+	},
+	data() {
+		return {
+			id: this.$route.params.id,
+			project: {},
+		}
+	},
+	methods: {
+		openSite() {
+			window.open(this.getCurrentProject.siteLink, "_blank")
+		},
+		openSource() {
+			window.open(this.getCurrentProject.sourceLink, "_blank")
+		},
+	},
+	beforeMount() {},
+	onUpdated() {
+		if (this.getCurrentProject.title) {
+			console.log("Current Project")
+			this.project = this.getCurrentProject()
+		} else {
+			console.log("Project by name")
+			this.project = this.getProjectByName(this.$route.params.id)
+		}
+	},
+	computed: {
+		...mapGetters(["getCurrentProject", "getProjectByName"]),
+		...mapGetters({
+			text: "getTexts",
+		}),
+	},
+}
 </script>
 
 <style scoped>
 h1 {
-  max-width: 300px;
-  text-align: left;
-  font-weight: var(--weight-normal);
-  font-size: var(--size-h1);
-  overflow: hidden;
+	max-width: 300px;
+	text-align: left;
+	font-weight: var(--weight-normal);
+	font-size: var(--size-h1);
+	overflow: hidden;
 }
 .title {
-  height: 100%;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
+	height: 100%;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: left;
 }
 .description {
-  max-width: 300px;
-  text-align: left;
-  font-size: var(--size-font-large);
-  font-weight: var(--weight-light);
-  overflow: hidden;
+	max-width: 300px;
+	text-align: left;
+	font-size: var(--size-font-large);
+	font-weight: var(--weight-light);
+	overflow: hidden;
 }
 .section {
-  background-color: var(--color-background-layout);
-  height: 100%;
-  width: 40%;
-  min-width: 500px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  backdrop-filter: blur(10px);
+	background-color: var(--color-background-layout);
+	height: 100%;
+	width: 40%;
+	min-width: 500px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	backdrop-filter: blur(10px);
 }
 .button-container {
-  max-width: 300px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  padding: 50px 20px;
-  gap: 20px;
+	max-width: 300px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: left;
+	padding: 50px 20px;
+	gap: 20px;
 }
 .project-container {
-  height: var(--container-height);
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+	height: var(--container-height);
+	display: flex;
+	flex-direction: row;
+	justify-content: left;
+	align-items: center;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
 }
 
 @media (max-width: 768px) {
-  .section {
-    width: 100%;
-    min-width: 0;
-    overflow: hidden;
-    backdrop-filter: blur(1px);
-  }
+	.section {
+		width: 100%;
+		min-width: 0;
+		overflow: hidden;
+		backdrop-filter: blur(1px);
+	}
 }
 @media (max-height: 600px) and (min-width: 560px) {
-  .section {
-    flex-direction: row;
-    width: auto;
-  }
+	.section {
+		flex-direction: row;
+		width: auto;
+	}
 }
 
 @media (max-width: 500px) {
-  .button-container {
-    max-width: none;
-  }
+	.button-container {
+		max-width: none;
+	}
 }
 @media only screen and (max-width: 500px) {
-  .button-container {
-    max-width: none;
-    padding-bottom: 120px;
-  }
+	.button-container {
+		max-width: none;
+		padding-bottom: 120px;
+	}
 }
 </style>
